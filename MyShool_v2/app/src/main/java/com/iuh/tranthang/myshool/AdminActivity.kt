0ea2549d7aafd67cf75cf9838981e8835fb4d184
Tran Thang <mt.tranthang@gmail.com>
2018-04-09 00:40:06 +0700

    Update BaseExpandableListAdapter
    - Display
    - Action click item child

--- MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/AdminActivity.kt
+++ MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/AdminActivity.kt
@@ -4,11 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.iuh.tranthang.myshool.ViewApdater.CustomAdapter
 import com.iuh.tranthang.myshool.ViewApdater.ExpandableListAdapter
 import com.iuh.tranthang.myshool.model.adm_display
 import kotlinx.android.synthetic.main.activity_admin.*
-import kotlinx.android.synthetic.main.activity_inside.*
 
 class AdminActivity : AppCompatActivity() {
 
@@ -18,33 +16,43 @@ class AdminActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
         //edit_password.setText(token_pw.getString("loginpassword"," "))
+
+        /* customer list view
         var arrayInforMenu: ArrayList<adm_display> = ArrayList()
-
         arrayInforMenu.add(adm_display("Thông tin nhân viên", R.drawable.team_group))
-
-        listview.adapter = CustomAdapter(this, arrayInforMenu)
-<<<<<<< HEAD
+        listview.adapter = CustomAdapter(this, arrayInforMenu)*/
 
 
-        val listHeader = listOf("number", "A")
-        val numberList = listOf("1", "2", "3")
+        val listHeader: ArrayList<adm_display> = ArrayList()
+        listHeader.add(adm_display("Thông tin nhân viên", R.drawable.team_group))
+        listHeader.add(adm_display("Quản lý thông báo", R.drawable.team_group))
+
+        val inforStaff: ArrayList<adm_display> = ArrayList()
+        inforStaff.add(adm_display("Danh sách tài khoản", R.drawable.team_group))
+        inforStaff.add(adm_display("Thêm tài khoản", R.drawable.team_group))
+        inforStaff.add(adm_display("Xóa nhân viên", R.drawable.team_group))
+
+        val inforNotify: ArrayList<adm_display> = ArrayList()
+        inforNotify.add(adm_display("Danh sách thông báo", R.drawable.team_group))
+        inforNotify.add(adm_display("Tạo thông báo", R.drawable.team_group))
+
         val fruitsList = listOf("Thang", "Tam")
-        val listChild = HashMap<String, List<String>>()
-        listChild.put(listHeader[0], numberList)
-        listChild.put(listHeader[1], fruitsList)
+        val listChild = HashMap<String, ArrayList<adm_display>>()
+
+        listChild.put(listHeader[0].getName(), inforStaff)
+        listChild.put(listHeader[1].getName(), inforNotify)
 
         val expandableListAdapter = ExpandableListAdapter(this, listHeader, listChild)
 
         expandable_list_view.setAdapter(expandableListAdapter)
-=======
+
         btnLogOut.setOnClickListener {
-            var editor= token.edit()
-            editor.putString("loginusername"," ")
+            var editor = token.edit()
+            editor.putString("loginusername", " ")
             editor.commit()
-            var intent= Intent(this, InsideActivity::class.java)
+            var intent = Intent(this, InsideActivity::class.java)
             startActivity(intent)
             finish()
         }
->>>>>>> 5ba91c4de5a57d782525a278fa6d98cbeacde043
     }
 }
--- MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/ViewApdater/ExpandableListAdapter.kt
+++ MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/ViewApdater/ExpandableListAdapter.kt
@@ -2,17 +2,21 @@ package com.iuh.tranthang.myshool.ViewApdater
 
 import android.content.Context
 import android.graphics.Typeface
+import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
 import com.iuh.tranthang.myshool.R
+import com.iuh.tranthang.myshool.model.adm_display
 
 /**
  * Created by ThinkPad on 4/8/2018.
  */
-class ExpandableListAdapter(val context: Context, val listOfHeaderData: List<String>, val listofChildData: HashMap<String, List<String>>) : BaseExpandableListAdapter() {
+class ExpandableListAdapter(val context: Context, val listOfHeaderData: ArrayList<adm_display>,
+                            val listofChildData: HashMap<String, ArrayList<adm_display>>) : BaseExpandableListAdapter() {
 
     override fun getGroup(position: Int): Any {
         return listOfHeaderData[position]
@@ -27,23 +31,26 @@ class ExpandableListAdapter(val context: Context, val listOfHeaderData: List<Str
     }
 
     override fun getGroupView(groupPosition: Int, isExpanded: Boolean, converView: View?, parent: ViewGroup?): View {
-        val headerTitle = getGroup(groupPosition) as String
+//        val headerTitle = getGroup(groupPosition) as String
+        val headerTitle = getGroup(groupPosition) as adm_display
+
         val view: View = LayoutInflater.from(context).inflate(R.layout.layout_list_header, parent, false)
         val listHeaderText = view.findViewById<AppCompatTextView>(R.id.list_header_text) as AppCompatTextView
+        val listHeaderImg = view.findViewById<AppCompatTextView>(R.id.list_header_img) as AppCompatImageView
         listHeaderText.setTypeface(null, Typeface.BOLD)
-        listHeaderText.text = headerTitle
+        listHeaderText.text = headerTitle.getName()
+        listHeaderImg.setImageResource(headerTitle.getImg())
 
         return view
 
     }
 
     override fun getChildrenCount(groupPosition: Int): Int {
-        return listofChildData[listOfHeaderData[groupPosition]]!!.size
-
+        return listofChildData[listOfHeaderData[groupPosition].getName()]!!.size
     }
 
     override fun getChild(groupPosition: Int, childPosition: Int): Any {
-        return listofChildData[listOfHeaderData[groupPosition]]!![childPosition]
+        return listofChildData[listOfHeaderData[groupPosition].getName()]!![childPosition]
     }
 
     override fun getGroupId(groupPosition: Int): Long {
@@ -51,10 +58,21 @@ class ExpandableListAdapter(val context: Context, val listOfHeaderData: List<Str
     }
 
     override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, converView: View?, parent: ViewGroup?): View {
-        val childText = getChild(groupPosition, childPosition) as String
+        val childText = getChild(groupPosition, childPosition) as adm_display
         val view: View = LayoutInflater.from(context).inflate(R.layout.layout_list_item, parent, false)
+
         val listItemText = view.findViewById<AppCompatTextView>(R.id.list_item_list) as AppCompatTextView
-        listItemText.text = childText
+        listItemText.text = childText.getName()
+
+        val listItemImg = view.findViewById<AppCompatImageView>(R.id.list_item_img) as AppCompatImageView
+        listItemImg.setImageResource(childText.getImg())
+
+        listItemImg.setOnClickListener(View.OnClickListener {
+            Log.e("tmt", childText.getName() + "")
+        })
+        listItemText.setOnClickListener(View.OnClickListener {
+            Log.e("tmt", childText.getName() + "")
+        })
 
         return view
     }
@@ -68,5 +86,6 @@ class ExpandableListAdapter(val context: Context, val listOfHeaderData: List<Str
         return listOfHeaderData.size
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 
 }
