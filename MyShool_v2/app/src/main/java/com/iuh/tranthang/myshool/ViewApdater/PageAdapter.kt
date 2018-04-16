eed2a44eb48a8a36ad58b238b8854a22c52fa887
nghiakt1996 <nghiakt1996@gmail.com>
2018-04-16 22:58:31 +0700

    FragmentProfile

---
+++ MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/ViewApdater/PageAdapter.kt
@@ -0,0 +1,22 @@
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class PageAdapter(fm:FragmentManager):FragmentPagerAdapter(fm){
+
+    var mFm = fm
+    var mFragmentItems:ArrayList<Fragment> = ArrayList()
+    var mFragmentTitle: ArrayList<String> = ArrayList()
+    fun addFragment(fragmentItem:Fragment,fragmentTitle:String){
+        mFragmentItems.add(fragmentItem)
+        mFragmentTitle.add(fragmentTitle)
+    }
+    override fun getItem(position: Int): Fragment {
+        return mFragmentItems[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentItems.size
+    }
+
+}
