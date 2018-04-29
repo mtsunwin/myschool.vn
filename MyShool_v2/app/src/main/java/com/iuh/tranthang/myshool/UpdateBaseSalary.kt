61371c700a6ef73b53e4741d9c1d48c3b69b08ca
nghiakt1996 <nghiakt1996@gmail.com>
2018-04-29 11:29:15 +0700

    final_UpdateBaseSalary

--- MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/UpdateBaseSalary.kt
+++ MyShool_v2/app/src/main/java/com/iuh/tranthang/myshool/UpdateBaseSalary.kt
@@ -7,8 +7,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.iuh.tranthang.myshool.model.Parameter
+import kotlinx.android.synthetic.main.activity_updateprofile.*
+import kotlinx.android.synthetic.main.fragment_dialogupdatebasesalary.*
 
 /**
  * Created by Administrator on 4/27/2018.
@@ -18,16 +21,31 @@ class UpdateBaseSalary : AppCompatActivity() {
     private var txtSalary_old: TextView? = null
     private var btnXacNhan: Button? = null
     private var txtHeSoLuongcoban: String? = ""
-
+    private var dbFireStore:FirebaseFirestore?=null
+    private var txtHeSoLuongCoBanOld:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_dialogupdatebasesalary)
-
         val db = FirebaseFirestore.getInstance()
 
         txtSalary = findViewById(R.id.txtHeSoLuongCoBan)
         txtSalary_old = findViewById(R.id.txtHeSoLuongCoBanOld)
         btnXacNhan = findViewById(R.id.btnXacNhanUpdateBaseSalary)
+        txtHeSoLuongCoBanOld= findViewById(R.id.txtHeSoLuongCoBanOld)
+        dbFireStore = FirebaseFirestore.getInstance()
+        dbFireStore!!.collection(Parameter().root_Luong).document("Salary")
+                    .get().addOnCompleteListener({ task ->
+                if (task.isSuccessful) {
+                    Log.e("Tmt inside", "mmmmmmmmmmmmmm")
+                    var result: DocumentSnapshot = task.result
+                    if (result.exists()) {
+                        txtHeSoLuongCoBanOld!!.text=result.data["LuongCoBan"].toString()
+                    } else {
+                        Log.e("tmt false", "false")
+                    }
+                }
+            })
+
         btnXacNhan!!.setOnClickListener {
             txtHeSoLuongcoban = txtSalary!!.text.toString()
             Log.e("Salary", txtSalary!!.text.toString())
@@ -37,10 +55,11 @@ class UpdateBaseSalary : AppCompatActivity() {
                 db.collection(Parameter().root_Luong)
                         .document("Salary").set(items)
                         .addOnSuccessListener { documentReference ->
-                            Toast.makeText(this, "Successful update", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "Cập nhật lương cơ bản thành công", Toast.LENGTH_SHORT).show()
+                            finish()
                         }
                 Log.e("Update", "Successful")
             }
-        }
+        }   
     }
 }
